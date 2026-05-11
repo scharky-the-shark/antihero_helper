0e601d985f84e9ba1369bfc5a9ece52693e73eab
Scharky <236237501+scharky-the-shark@users.noreply.github.com>
2026-05-11 13:56:56 +0200

    Update messageCreate1.js

--- events/messageCreate1.js
+++ events/messageCreate1.js
@@ -1,13 +1,1 @@
-const automod = require("../utils/automod");
-const spamProtection = require("../utils/spamProtection");
 
-module.exports = {
-  name: "messageCreate",
-
-  async execute(message, client) {
-
-    await automod(message, client);
-    await spamProtection(message, client);
-
-  }
-};
