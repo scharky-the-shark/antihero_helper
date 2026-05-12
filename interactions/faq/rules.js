671d0640099413b5a6d769c2c031c154663b1831
Scharky <236237501+scharky-the-shark@users.noreply.github.com>
2026-05-12 22:12:30 +0200

    Create rules.js

---
+++ interactions/faq/rules.js
@@ -0,0 +1,11 @@
+module.exports = async (interaction) => {
+
+    return interaction.reply({
+      ephemeral: true,
+      content:
+        "### Codex of Rule for all Antiheros\n" +
+        ">>> 1. [TOS Antihero Studios](<https://www.antiherostudios.com/terms>)\n" +
+        "2. [Discord TOS](<https://discord.com/terms>)\n" +
+        "3. [Serverrules](https://discord.com/channels/1296481397674082374/1338819041048924251)"
+    });
+  }
